15dacb84428e202248750a93dd84505e8863c722
Engel02 <73982270+Engel02@users.noreply.github.com>
2024-03-24 20:21:59 +0000

    Edu

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,7 +18,7 @@ import React from "react";
  */
 import image from "../images/backImage.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "A galaxy";
 
 /**
  * Sort description that expands on your title on the Home component.
